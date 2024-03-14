7cd8f2da832184954358104ebdeb4539a9803309
Disha-Tangade <144653555+DishaTangade@users.noreply.github.com>
2024-03-14 22:17:28 +0530

    Add files via upload

---
+++ OOPJ/Assignment3/Assignment3.java
@@ -0,0 +1,11 @@
+package SecondProgram;
+
+public class Assignment3{
+	public static void main(String[] args) {
+		int num1=10;
+		double num2=num1;
+		System.out.println(num1+" "+num2);
+	}
+
+}
+//output: num1:10 num2:10.0
